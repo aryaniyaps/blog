20f2c4314ddf961a3053773ce8ee74ca28c785ce
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-11-05 16:33:58 +0530

    update blog

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,4 +1,4 @@
-import { Inter } from 'next/font/google'
+import { Inter, Rubik } from 'next/font/google'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import { ReactNode } from 'react'
@@ -8,14 +8,14 @@ interface Props {
   children: ReactNode
 }
 
-const inter = Inter({
+const rubik = Rubik({
   subsets: ['latin'],
 })
 
 const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
-      <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
+      <div className={`${rubik.className} flex h-screen flex-col justify-between font-sans`}>
         <Header />
         <main className="mb-auto">{children}</main>
         <Footer />
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
+  { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ]
 
