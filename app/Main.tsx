706ff4ad8a23ecb44e61dd93f56f240ae4fedf64
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-11-23 13:24:37 +0530

    add link to all blog posts

--- app/Main.tsx
+++ app/Main.tsx
@@ -14,7 +14,34 @@ export default function Home({ posts }) {
     <>
       <div className="flex h-full flex-col items-center px-3 sm:min-h-screen">
         <Hero />
-
+        <div className="flex w-full items-center justify-center py-4">
+          <Link href="/blog">
+            <button className="bg-background-800 shadow-background-900 group relative inline-block cursor-pointer rounded-full p-px text-xs font-semibold leading-6 text-foreground  no-underline shadow-2xl">
+              <span className="absolute inset-0 overflow-hidden rounded-full">
+                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+              </span>
+              <div className="bg-background-950 relative z-10 flex items-center space-x-2 rounded-full px-4 py-0.5 ring-1 ring-foreground/10 ">
+                <span>{`All Blog Posts`}</span>
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                    d="M10.75 8.75L14.25 12L10.75 15.25"
+                  ></path>
+                </svg>
+              </div>
+              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-sky-400/0 via-sky-400/90 to-sky-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+            </button>
+          </Link>
+        </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
@@ -78,19 +105,6 @@ export default function Home({ posts }) {
           })}
         </ul>
       </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex w-full flex-col items-center justify-center py-2">
-          <Link href="/blog">
-            <HoverBorderGradient
-              containerClassName="rounded-full"
-              as="span"
-              className="flex items-center space-x-2 bg-white py-1 text-black dark:bg-black dark:text-white"
-            >
-              Latest Blog Posts
-            </HoverBorderGradient>
-          </Link>
-        </div>
-      )}
       {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
