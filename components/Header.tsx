a9b9fcb7a5e0f03def669c37106669477ad62691
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-11-25 16:49:57 +0530

    update favicon and logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,23 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-} from '@nextui-org/react'
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react'
+import Logo from '@/data/logo.svg'
 
 const Header = () => {
   return (
     <Navbar isBordered isBlurred={false}>
       <NavbarBrand className="flex items-center">
         {typeof siteMetadata.headerTitle === 'string' ? (
-          <Link href={'/'} color="foreground" className="font-bold text-inherit">
-            {siteMetadata.headerTitle}
+          <Link href={'/'}>
+            <div className="mr-3">
+              <Logo />
+            </div>
+            <p className="font-bold text-inherit">{siteMetadata.headerTitle}</p>
           </Link>
         ) : (
           siteMetadata.headerTitle
